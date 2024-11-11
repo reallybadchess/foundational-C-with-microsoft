0b7975fa4a05d31f14e72c9f6bf0550d64725f4f
reallybadchess <m.raphael150@gmail.com>
2024-11-10 19:35:57 -0700

    fixed syntax

--- CsharpProjects/DiceProject/Program.cs
+++ CsharpProjects/DiceProject/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Random dice = new Random();
+﻿Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -10,18 +8,17 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)
-{ 
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
     total += 2;
 }
 
-if (roll1 == roll2) && (roll2 == roll3)
-{ 
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
     Console.WriteLine("You rolled triples! +6 bonus to total!");
     total += 6;
 }
-
 
 if (total >= 15)
 {
@@ -30,5 +27,5 @@ if (total >= 15)
 
 if (total < 15)
 {
-    Console.WriteLine("Loser.");  
-}  
+    Console.WriteLine("Loser.");
+}
--- CsharpProjects/DiceProject/obj/Debug/net8.0/DiceProject.AssemblyInfo.cs
+++ CsharpProjects/DiceProject/obj/Debug/net8.0/DiceProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366db33f7048e938d08e4575cfbe7faddf5674c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f67e1e6288020c2a84f797fbef2364c26389ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
