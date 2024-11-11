08eae19001ddec11bbbb29d82554a571090c7c86
reallybadchess <m.raphael150@gmail.com>
2024-11-10 19:30:57 -0700

    purposeful bugs

--- CsharpProjects/DiceProject/Program.cs
+++ CsharpProjects/DiceProject/Program.cs
@@ -10,12 +10,17 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (total > 14)
+if (roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)
+{ 
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+if (total >= 15)
 {
     Console.WriteLine("You win!");
 }
 
 if (total < 15)
 {
-    Console.WriteLine("Loser.");
-}
+    Console.WriteLine("Loser.");  
+}  
