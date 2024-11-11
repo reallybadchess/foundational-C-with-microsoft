9f67e1e6288020c2a84f797fbef2364c26389ae0
reallybadchess <m.raphael150@gmail.com>
2024-11-10 19:33:57 -0700

    adding the triples bonus, dotnet build wont execute properly.

--- CsharpProjects/DiceProject/Program.cs
+++ CsharpProjects/DiceProject/Program.cs
@@ -15,6 +15,14 @@ if (roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
     total += 2;
 }
+
+if (roll1 == roll2) && (roll2 == roll3)
+{ 
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+
 if (total >= 15)
 {
     Console.WriteLine("You win!");
